5c9c8f0720becf4954595d1873d31642ff85724d
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2023-02-25 17:07:58 +0900

    依存関係を追加

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
 // scrapbox-userscript-std（fork版）
 export {
   getCodeBlocks,
+  updateCodeBlock,
 } from "https://raw.githubusercontent.com/MijinkoSD/scrapbox-userscript-std/set-codeblock/mod.ts";
 export type {
   TinyCodeBlock,
@@ -11,3 +12,9 @@ import type {
   Scrapbox,
 } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.8/scrapbox.ts";
 export declare const scrapbox: Scrapbox;
+
+// input-dialog-userscript
+export {
+  buildInAlertModes,
+  scrapboxAlert,
+} from "https://raw.githubusercontent.com/MijinkoSD/input-dialog-userscript/v1.1.0/scrapboxAlert.ts";
